c30e3d0bb32aa069ccf9bcdb85daa72f87f5570f
Olesya Krayanskaya <krayanskaya_ov@mail.ru>
2022-12-07 21:21:46 +0300

    slider

--- module1/index.js
+++ module1/index.js
@@ -20,12 +20,14 @@ function slideRight() {
     if (slideNumber >= slides.length) {
         slides[slideNumber - 1].classList.remove('testimonials__slide_active')
         sliderDots[slideNumber - 1].classList.remove('testimonial__dot_active')
+        slides[slideNumber - 1].classList.remove('testimonials__slide_animated')
         slideNumber = 0
         slides[slideNumber].classList.add('testimonials__slide_active')
         sliderDots[slideNumber].classList.add('testimonial__dot_active')
     } else {
         slides[slideNumber - 1].classList.remove('testimonials__slide_active')
         sliderDots[slideNumber - 1].classList.remove('testimonial__dot_active')
+        slides[slideNumber - 1].classList.remove('testimonials__slide_animated')
         slides[slideNumber].classList.add('testimonials__slide_active')
         sliderDots[slideNumber].classList.add('testimonial__dot_active')
     }
@@ -35,12 +37,14 @@ function slideLeft() {
     if (slideNumber <= 0) {
         slides[slideNumber].classList.remove('testimonials__slide_active')
         sliderDots[slideNumber].classList.remove('testimonial__dot_active')
+        slides[slideNumber].classList.remove('testimonials__slide_animated')
         slideNumber = slides.length - 1
         slides[slideNumber].classList.add('testimonials__slide_active')
         sliderDots[slideNumber].classList.add('testimonial__dot_active')
     } else {
         slides[slideNumber].classList.remove('testimonials__slide_active')
         sliderDots[slideNumber].classList.remove('testimonial__dot_active')
+        slides[slideNumber].classList.remove('testimonials__slide_animated')
         slideNumber--
         slides[slideNumber].classList.add('testimonials__slide_active')
         sliderDots[slideNumber].classList.add('testimonial__dot_active')
@@ -59,22 +63,39 @@ function findActiveSlide() {
 
 arrLeft.addEventListener('click', function () {
     slideLeft(slides)
+    if (window.matchMedia('(min-width: 993px)').matches) {
+        let i = findActiveSlide()
+        let shiftSlides = i * (-1 * slides[i].offsetWidth) + 'px'
+        slidesContainer.style.transform = "translateX(" + shiftSlides + ")"
+        slidesContainer.style.transition = "transform 1s ease-in"
+    }
+    if (window.matchMedia('(max-width: 992px)').matches) {
+        let i = findActiveSlide()
+        slides[i].classList.add('testimonials__slide_animated')
+    }
 })
 
 arrRight.addEventListener('click', function () {
     slideRight(slides)
     if (window.matchMedia('(min-width: 993px)').matches) {
         let i = findActiveSlide()
+        slides[0].classList.remove('testimonials__slide_large-animated-right')
         if (i >= slides.length) {
             i = 0
         }
-        let shiftSlides = i * (-0.25 * slidesContainer.offsetWidth) + 'px'
+        let shiftSlides = i * (-1 * slides[i].offsetWidth) + 'px'
         slidesContainer.style.transform = "translateX(" + shiftSlides + ")"
-        console.log(shiftSlides)
+        slidesContainer.style.transition = "transform 1s ease-in"
+        if (i <= 0) {
+            slides[0].classList.add('testimonials__slide_large-animated-right')
+        }
+    }
+    if (window.matchMedia('(max-width: 992px)').matches) {
+        let i = findActiveSlide()
+        slides[i].classList.add('testimonials__slide_animated')
     }
 })
 
-console.log(slidesContainer.offsetWidth)
 
 
 
